31c07eebf2f3f919293ef85e098420608124b2b2
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-17 18:04:28 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -12,3 +12,4 @@ end
 # You should be able to read and write to these attributes. This class should
 # also keep track of each instance of dog created, as well as a class method
 # `all` to return an array of those instances.
+gret= dog.new("gret","red","5")
