ae10f1f193c553b9dd377d76a688b36788f49082
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-17 17:58:38 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,4 +1,10 @@
 class Dog 
+  
   def initialize(name,age)
   # Replace with Dog clas
 end
+# Create a new file in the models directory to create a dog class. This class
+# should have name, breed, and age attributes which can be set on initialization.
+# You should be able to read and write to these attributes. This class should
+# also keep track of each instance of dog created, as well as a class method
+# `all` to return an array of those instances.
